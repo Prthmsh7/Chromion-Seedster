f02067db860dae656af13a96ff5296f6bb665bfe
Prathmesh Shukla <115979305+Prthmsh7@users.noreply.github.com>
2025-06-29 00:31:47 +0530

    Fix Reference Folder Dependencies

--- reference/src/main.tsx
+++ reference/src/main.tsx
@@ -2,12 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { ReactLenis, useLenis } from "lenis/react";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ReactLenis root>
-      <App />
-    </ReactLenis>
+    <App />
   </React.StrictMode>
 );
